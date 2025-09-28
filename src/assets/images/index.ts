c18d54d284d20cae499af5b2a8ae29dc6c3e260e
Ha Quanq <quangha0120010@gmail.com>
2025-09-29 00:05:06 +0700

    Add static data and images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export { default as HeaderBackgroundDesktop } from "./bg-header-desktop.svg";
+export { default as HeaderBackgroundMobile } from "./bg-header-mobile.svg";
+export { default as RemoveIcon } from "./icon-remove.svg";

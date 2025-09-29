b63cb11e4f4d9e1ad7183ad61fc6a514b6f35e7c
Ha Quanq <quangha0120010@gmail.com>
2025-09-29 13:53:15 +0700

    Add header pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { HeaderBackgroundDesktop, HeaderBackgroundMobile } from "./assets/images";
+
 export const App = () => {
-  return <></>;
+  return (
+    <>
+      <main class="min-h-screen bg-green-50">
+        <h1 class="sr-only">This is just an example</h1>
+        <div class="h-39 overflow-hidden bg-green-400 *:w-full *:object-cover">
+          <img class="md:hidden" src={HeaderBackgroundMobile} alt="" />
+          <img class="hidden h-full md:block" src={HeaderBackgroundDesktop} alt="" />
+        </div>
+      </main>
+    </>
+  );
 };
